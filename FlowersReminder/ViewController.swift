075947fae1031774219c44efb971840b87633940
Artem <t-e-m-a_@mail.ru>
2022-09-06 14:03:51 +0300

    Initial Commit

---
+++ FlowersReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlowersReminder
+//
+//  Created by Artem Vorobev on 06.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
