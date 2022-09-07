4d9be7de671a625fd268848282a934c6aa67a7a4
Artem <t-e-m-a_@mail.ru>
2022-09-07 17:37:19 +0300

    Add ActiveAlarmsCell

--- FlowersReminder/Controllers/ActiveAlarms.swift
+++ FlowersReminder/Controllers/ActiveAlarms.swift
@@ -11,6 +11,34 @@ import RealmSwift
 
 class ActiveAlarms: UIViewController{
     
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+       let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    let openCalendarButton: UIButton = {
+        let openCalendarButton = UIButton()
+        openCalendarButton.setTitle("Открыть календарь", for: .normal)
+        openCalendarButton.setTitleColor(.black, for: .normal)
+        openCalendarButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        openCalendarButton.translatesAutoresizingMaskIntoConstraints = false
+        return openCalendarButton
+    }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let activeAlarmsCellId = "activeAlarmsCellId"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewControllerConfig()
@@ -38,5 +66,87 @@ class ActiveAlarms: UIViewController{
                         } else {
                             navigationController?.navigationBar.barTintColor = UIColor(hexString: "#ECFBDE")
                         }
+        
+        setConstraints()
+        
+        calendar.dataSource = self
+        calendar.delegate = self
+        calendar.scope = .week
+        openCalendarButton.addTarget(self, action: #selector(openCalendarButtonTapped), for: .touchUpInside)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = UIColor(hexString: "#FBDDE7")
+        tableView.register(ActiveAlarmsCell.self, forCellReuseIdentifier: activeAlarmsCellId)
+        
     }
+    
+    @objc func openCalendarButtonTapped(){
+        if calendar.scope == .week{
+            calendar.scope = .month
+            openCalendarButton.setTitle("Закрыть календарь", for: .normal)
+        }else{
+            calendar.scope = .week
+            openCalendarButton.setTitle("Открыть календарь", for: .normal)
+        }
+    }
+}
+
+
+extension ActiveAlarms: FSCalendarDelegate, FSCalendarDataSource, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: activeAlarmsCellId, for: indexPath) as! ActiveAlarmsCell
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    
+    //MARK: Set constraints
+    
+    func setConstraints(){
+        view.addSubview(calendar)
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250)
+        calendar.addConstraint(calendarHeightConstraint)
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        ])
+        view.addSubview(openCalendarButton)
+        NSLayoutConstraint.activate([
+            openCalendarButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 5),
+            openCalendarButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            openCalendarButton.widthAnchor.constraint(equalToConstant: 150)
+        ])
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: openCalendarButton.bottomAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+    
+    //MARK: FSCalendar delegate and dataSource
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+    
 }
---
+++ FlowersReminder/Models/Cells/ActiveAlarmsCell.swift
@@ -0,0 +1,132 @@
+//
+//  ActiveAlarmsCell.swift
+//  FlowersReminder
+//
+//  Created by Artem Vorobev on 07.09.2022.
+//
+
+import UIKit
+
+class ActiveAlarmsCell: UITableViewCell {
+    
+    let hStack: UIStackView = {
+       let hStack = UIStackView()
+        hStack.axis = .horizontal
+        hStack.distribution = .fillProportionally
+        hStack.backgroundColor = .lightGray
+        hStack.layer.cornerRadius = 10
+        hStack.layer.masksToBounds = true
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        return hStack
+    }()
+    
+    let flowerImage: UIImageView = {
+       let flowerImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+        flowerImage.image = UIImage(named: "defaultFlower")
+        flowerImage.contentMode = .scaleAspectFit
+        
+        
+        flowerImage.translatesAutoresizingMaskIntoConstraints = false
+        return flowerImage
+    }()
+    
+    let vStack: UIStackView = {
+       let vStack = UIStackView()
+        vStack.axis = .vertical
+        vStack.distribution = .fillEqually
+        vStack.backgroundColor = .lightGray
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        return vStack
+    }()
+    
+    let flowerName: UILabel = {
+       let flowerName = UILabel()
+        flowerName.text = "Гербера"
+        flowerName.adjustsFontSizeToFitWidth = true
+        
+        flowerName.translatesAutoresizingMaskIntoConstraints = false
+        return flowerName
+    }()
+    
+    let flowerStatus: UILabel = {
+       let flowerStatus = UILabel()
+        flowerStatus.text = "Cегодня полить и удобрить"
+        flowerStatus.adjustsFontSizeToFitWidth = true
+        flowerStatus.translatesAutoresizingMaskIntoConstraints = false
+        return flowerStatus
+    }()
+    
+    let compliteButton: UIButton = {
+       let compliteButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        compliteButton.setImage(UIImage(named: "complite"), for: .normal)
+        
+        compliteButton.translatesAutoresizingMaskIntoConstraints = false
+        return compliteButton
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.backgroundColor = UIColor(hexString: "#FBDDE7")
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   
+    
+    //MARK: Set Constraints
+    
+    private func setConstraints(){
+        
+        self.contentView.addSubview(compliteButton)
+        NSLayoutConstraint.activate([
+            compliteButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            compliteButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+        ])
+        
+        self.addSubview(hStack)
+        NSLayoutConstraint.activate([
+            hStack.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+            hStack.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5),
+            hStack.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            hStack.trailingAnchor.constraint(equalTo: compliteButton.leadingAnchor, constant: -10),
+        ])
+        
+        hStack.addSubview(flowerImage)
+        NSLayoutConstraint.activate([
+            flowerImage.topAnchor.constraint(equalTo: hStack.topAnchor, constant: 0),
+            flowerImage.bottomAnchor.constraint(equalTo: hStack.bottomAnchor, constant: 0),
+            flowerImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            flowerImage.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+        hStack.addSubview(vStack)
+        NSLayoutConstraint.activate([
+            vStack.topAnchor.constraint(equalTo: hStack.topAnchor, constant: 0),
+            vStack.bottomAnchor.constraint(equalTo: hStack.bottomAnchor, constant: 0),
+            vStack.leadingAnchor.constraint(equalTo: flowerImage.trailingAnchor, constant: 0),
+            vStack.trailingAnchor.constraint(equalTo: hStack.trailingAnchor, constant: 0),
+        ])
+        
+        vStack.addSubview(flowerName)
+        NSLayoutConstraint.activate([
+            flowerName.topAnchor.constraint(equalTo: vStack.topAnchor, constant: 5),
+            flowerName.leadingAnchor.constraint(equalTo: vStack.leadingAnchor, constant: 0),
+            flowerName.trailingAnchor.constraint(equalTo: vStack.trailingAnchor, constant: -5),
+            flowerName.heightAnchor.constraint(equalToConstant: 35),
+        ])
+        
+        vStack.addSubview(flowerStatus)
+        NSLayoutConstraint.activate([
+            flowerStatus.topAnchor.constraint(equalTo: flowerName.bottomAnchor, constant: 5),
+            flowerStatus.leadingAnchor.constraint(equalTo: vStack.leadingAnchor, constant: 0),
+            flowerStatus.trailingAnchor.constraint(equalTo: vStack.trailingAnchor, constant: -5),
+            flowerStatus.bottomAnchor.constraint(equalTo: vStack.bottomAnchor, constant: 0),
+        ])
+        
+    }
+
+}
